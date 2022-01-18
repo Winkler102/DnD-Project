ab99438196cd703300d18a3f3096dd7d57fcba3d
Benjamin Winkler <benjiwinkler@yahoo.com>
2022-01-18 16:31:11 -0600

    base files

---
+++ client/src/components/Table/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+function Table() {
+    return (
+        <div>Test</div>
+    );
+}
+
+export default Table;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Table from "../components/Table";
 
 const Home = () => {
     return (
         <div className="container">
-            <p>Test</p>
+            <Table></Table>
         </div>
     );
 };
