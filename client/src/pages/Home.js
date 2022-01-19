c849d5d6f1490067778f0dbadeace926835e035c
Benjamin Winkler <benjiwinkler@yahoo.com>
2022-01-18 18:45:44 -0600

    created basic frag and drop

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Table from "../components/Table";
+import Token from "../components/Token";
+import "./style.css";
 
 const Home = () => {
     return (
         <div className="container">
-            <Table></Table>
+            <div className="flexbox">
+                <Table id='table-1' className='table'>
+                    <Token id='token-1' className='token' draggable='true'>
+                        <p>Token1</p>
+                    </Token>
+                </Table>
+
+                <Table id='table-2' className='table'>
+                    <Token id='token-2' className='token' draggable='true'>
+                        <p>Token 2</p>
+                    </Token>
+                </Table>
+                
+            </div>
         </div>
     );
 };
